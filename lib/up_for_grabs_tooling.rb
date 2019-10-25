bc5734b2fb62dc35a4a20cdb3c900b6a336e0881
Brendan Forster <github@brendanforster.com>
2019-10-24 21:48:03 -0300

    version bump

--- lib/up_for_grabs_tooling.rb
+++ lib/up_for_grabs_tooling.rb
@@ -4,3 +4,7 @@ require 'safe_yaml'
 
 require 'models/project_with_stats'
 require 'models/project'
+
+require 'validators/data_files'
+require 'validators/directory'
+require 'validators/project'
---
+++ lib/validators/data_files.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require_relative 'project'
+
+# Validate the data files
+class DataFilesValidator
+  def self.check(root, schemer)
+    projects = Dir["#{root}/_data/projects/*.yml"].map do |f|
+      relative_path = Pathname.new(f).relative_path_from(root).to_s
+      Project.new(relative_path, f)
+    end
+
+    projects_with_errors = []
+    projects_without_issues = []
+
+    projects.each do |p|
+      validation_errors = ProjectValidator.validate(p, schemer)
+      if validation_errors.empty?
+        projects_without_issues << [p, nil]
+      else
+        projects_with_errors << [p, validation_errors]
+      end
+    end
+
+    if projects_with_errors.any?
+      puts "#{projects_with_errors.count} errors found processing projects:"
+      projects_with_errors.each do |project, errors|
+        puts " - #{project.relative_path}:"
+        errors.each { |error| puts "    - #{error}" }
+      end
+      exit(-1)
+    else
+      puts "#{projects_without_issues.count} files processed - no errors found!"
+    end
+  end
+end
