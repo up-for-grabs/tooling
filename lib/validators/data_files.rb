7455c43e9f432fbb86bf70a692b7ec87bb60180f
Brendan Forster <github@brendanforster.com>
2019-11-18 11:12:58 -0400

    make schemer optional to simplify internals (#17)

--- lib/validators/data_files.rb
+++ lib/validators/data_files.rb
@@ -4,13 +4,19 @@ require_relative 'project'
 
 # Validate the data files
 class DataFilesValidator
-  def self.validate(root, schemer)
+  def self.validate(root, schemer = nil)
     projects = Dir["#{root}/_data/projects/*.yml"].map do |f|
       relative_path = Pathname.new(f).relative_path_from(root).to_s
       Project.new(relative_path, f)
     end
 
     projects_with_errors = {}
+
+    if schemer.nil?
+      root = File.dirname(File.dirname(__dir__))
+      schema = Pathname.new("#{root}/schema.json")
+      schemer = JSONSchemer.schema(schema)
+    end
 
     projects.each do |p|
       validation_errors = ProjectValidator.validate(p, schemer)
--- test/validators/data_files_test.rb
+++ test/validators/data_files_test.rb
@@ -4,20 +4,18 @@ require_relative '../test_helper'
 
 class DataFilesValidatorTests < Minitest::Test
   def test_valid_directory
-    schemer = setup_schemer
     path = get_directory('valid_project_files')
 
-    result = DataFilesValidator.validate(path, schemer)
+    result = DataFilesValidator.validate(path)
 
     assert_equal 3, result[:count]
     assert result[:errors].empty?
   end
 
   def test_file_has_error
-    schemer = setup_schemer
     path = get_directory('one_file_with_error')
 
-    result = DataFilesValidator.validate(path, schemer)
+    result = DataFilesValidator.validate(path)
 
     assert_equal 2, result[:count]
     assert_equal 1, result[:errors].length
@@ -25,12 +23,6 @@ class DataFilesValidatorTests < Minitest::Test
     errors = result[:errors]['_data/projects/error_site_link_url.yml']
     assert_equal 1, errors.length
     assert_equal errors[0], "Field '/site' expects a URL but instead found 'foo'. Please check and update this value."
-  end
-
-  def setup_schemer
-    root = File.dirname(File.dirname(__dir__))
-    schema = Pathname.new("#{root}/schema.json")
-    JSONSchemer.schema(schema)
   end
 
   def get_directory(name)
