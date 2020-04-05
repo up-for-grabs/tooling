ed82d2f96fcad5a8953b1797e23158c03a43bdf3
Brendan Forster <github@brendanforster.com>
2020-04-05 18:26:22 -0300

    other comment for project

--- lib/validators/command_line.rb
+++ lib/validators/command_line.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# This class validates the repository state from the command line, and performs
+# several checks during this process:
+#
+#  - schema validation for all current projects
+#  - tag validation
+#  - directory structure validation
+#
 class CommandLineValidator
   def self.validate(root, schemer = nil)
     projects = Dir["#{root}/_data/projects/*.yml"].map do |f|
