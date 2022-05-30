001327bc1a93e0cf553117d466e5c2403c0dba56
Brendan Forster <github@brendanforster.com>
2022-05-30 13:42:51 -0300

    address reported problems with predicates

--- test/models/github_matching_test.rb
+++ test/models/github_matching_test.rb
@@ -6,35 +6,35 @@ class GitHubMatchingTests < Minitest::Test
   def test_finds_github_repo
     project = create_project('valid_github_project.yml')
 
-    assert project.github_project?
+    assert_predicate(project, :github_project?)
     assert_equal 'up-for-grabs/up-for-grabs.net', project.github_owner_name_pair
   end
 
   def test_ignores_organization
     project = create_project('invalid_github_organization.yml')
 
-    refute project.github_project?
+    refute_predicate(project, :github_project?)
     assert_nil project.github_owner_name_pair
   end
 
   def test_ignores_external_site
     project = create_project('invalid_external_link.yml')
 
-    refute project.github_project?
+    refute_predicate(project, :github_project?)
     assert_nil project.github_owner_name_pair
   end
 
   def test_ignores_invalid_link
     project = create_project('invalid_link_not_handled.yml')
 
-    refute project.github_project?
+    refute_predicate(project, :github_project?)
     assert_nil project.github_owner_name_pair
   end
 
   def test_ignores_gitlab
     project = create_project('invalid_gitlab_link.yml')
 
-    refute project.github_project?
+    refute_predicate(project, :github_project?)
     assert_nil project.github_owner_name_pair
   end
 
